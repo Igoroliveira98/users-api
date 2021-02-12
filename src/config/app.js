20b96554ec8d2635723094c0123c532dac5eb688
Igor Oliveira <igoroliveira2798@gmail.com>
2021-02-11 21:40:56 -0300

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -1,20 +1,22 @@
+// Server Config
+
 const express = require("express");
 
-class App {
-    contructor() {
-        this.express = express()
+class AppController {
+  constructor() {
+    this.express = express();
 
-        this.middlewares();
-        this.routes();
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.express.use(express.json())
-    }
+  middlewares() {
+    this.express.use(express.json());
+  }
 
-    routes() {
-        this.express.use(require('../routes'))
-    }
+  routes() {
+    this.express.use(require("../routes"));
+  }
 }
 
-module.exports = new App().express;
+module.exports = new AppController().express;
