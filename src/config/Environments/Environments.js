b73eac6e22d4e3f015ecb906671f4d3fb4320ee7
Igor Oliveira <igoroliveira2798@gmail.com>
2021-02-17 19:04:58 -0300

    Update environment.js

--- src/config/Environments/Environments.js
+++ src/config/Environments/Environments.js
@@ -4,5 +4,6 @@ module.exports = {
     dbName: process.env.DB_NAME,
     dbUser: process.env.DB_USER,
     dbPass: process.env.DB_PASS,
-    port: process.env.PORT
+    port: process.env.PORT,
+    dbTestDialect: process.env.DB_DIALECT
 }
