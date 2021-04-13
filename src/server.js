0250bd35abbc161b301f5b7d11fa9bc1aaf38eac
igor <igoroliveira2798@gmail.com>
2021-04-12 22:01:55 -0300

    updated server.js

--- src/server.js
+++ src/server.js
@@ -7,10 +7,13 @@ const cors = require("cors")
 const app = express();
 
 const routes = require("./routes");
+const Error = require("./middlewares/Error");
 
 app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use(Error);
 
 app.listen(3333, () => console.log("server is running!!"));
